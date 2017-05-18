23dfb4af4b8637652f82c7fb421ca00beca60f82
Pepan7 <pepanpeter@gmail.com>
2017-05-18 17:59:15 +0200

    zaciatok intranetu2

---
+++ intranet.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="sk">
+<head>
+    <meta charset="utf-8">
+    <title>Intranet</title>
+    <link type="text/css" rel="stylesheet" href="Styly stranky/dochadzka.css">
+    <link type="text/css" rel="stylesheet" href="http://netdna.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="Javascripty/.js"></script>
+</head>
+<body>
+</body>
+</html>
